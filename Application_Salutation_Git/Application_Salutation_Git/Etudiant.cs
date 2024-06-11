fd5fd586e223be4faa6493240813427541d265f8
licenceisesod <finalisterritisesod2024@gmail.com>
2024-06-11 09:25:36 +0200

    Création classe étudiant

---
+++ Application_Salutation_Git/Application_Salutation_Git/Etudiant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Salutation_Git
+{
+    class Etudiant
+    {
+        private String idEt;
+
+        public String IdEt
+        {
+            get { return idEt; }
+            set { idEt = value; }
+        }
+        private String nom;
+
+        public String Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public Etudiant()
+        {
+        }
+    }
+}
